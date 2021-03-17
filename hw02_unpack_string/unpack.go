621d1ff0374d3322244e598ab1abefc85aa73691
sobolev <gasobolev@gmail.com>
2021-03-17 18:39:12 +0300

    main objective completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -23,12 +23,14 @@ func Unpack(s string) (string, error) {
 				ErrInvalidString = errors.New("invalid string")
 			} else if unicode.IsDigit(ra[i-1]) == true {
 				ErrInvalidString = errors.New("invalid string")
-			} else if e == '0' {
-				a = strings.TrimSuffix(a, string(ra[i-1]))
 			} else {
-				x, _ := strconv.Atoi(string(e))
-				for c := 1; c < x; c++ {
-					a = a + string(ra[i-1])
+				if e == '0' {
+					a = strings.TrimSuffix(a, string(ra[i-1]))
+				} else {
+					x, _ := strconv.Atoi(string(e))
+					for c := 1; c < x; c++ {
+						a = a + string(ra[i-1])
+					}
 				}
 			}
 		}
